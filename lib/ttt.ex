ff11361f7ab1a5888723c609335ccb59125f8710
Seth <sthomas@8thlight.com>
2022-05-31 09:39:15 -0400

    Skeleton (#1)
    * [FEATURE] Added CI & Skeleton
    
    * [FEATURE] Started new project

---
+++ lib/ttt.ex
@@ -0,0 +1,18 @@
+defmodule Ttt do
+  @moduledoc """
+  Documentation for `Ttt`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Ttt.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
